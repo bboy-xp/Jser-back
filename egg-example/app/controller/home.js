1a117a3149f682bdea125eeae111b82df2c2b7e1
xp-12138 <1169470657@qq.com>
2018-05-01 15:51:25 +0800

    后端数据已存入数据库

--- egg-example/app/controller/home.js
+++ egg-example/app/controller/home.js
@@ -8,6 +8,23 @@ class HomeController extends Controller {
   }
   async postUsermsg() {
     const ctx = this.ctx;
+    const Usermsg = ctx.model.Usermsg;
+    // console.log(ctx.request.body);
+    const data = ctx.request.body;
+    const usermsg = new Usermsg({
+      name: data.name,
+      sex: data.sex,
+      grade: data.grade,
+      major: data.major,
+      college: data.college,
+      phoneNum: data.phoneNum,
+      level: data.level,
+      reason: data.reason,
+      introduce: data.introduce,
+      otherReason: data.otherReason,
+    });
+    usermsg.save();
+
     ctx.body = 'ok';
   }
 }
