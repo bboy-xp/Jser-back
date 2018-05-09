401bebdf7f6a123c69f3094ee932d566c69df067
unknown <1169470657@qq.com>
2018-05-09 11:35:23 +0800

    新增update方法

--- egg-example/app/controller/home.js
+++ egg-example/app/controller/home.js
@@ -12,21 +12,54 @@ class HomeController extends Controller {
     const Usermsg = ctx.model.Usermsg;
     // console.log(ctx.request.body);
     const data = ctx.request.body;
-    const usermsg = new Usermsg({
-      openId: data.openId,
-      name: data.name,
-      sex: data.sex,
-      grade: data.grade,
-      major: data.major,
-      college: data.college,
-      phoneNum: data.phoneNum,
-      level: data.level,
-      reason: data.reason,
-      introduce: data.introduce,
-      otherReason: data.otherReason,
-    });
-    usermsg.save();
-
+    const haveUser = await new Promise((resolve,reject) => {
+      Usermsg.findOne({openId: data.openId},(err,doc) => {
+        if(doc){
+          resolve(true);
+        }else{
+          resolve(false);
+        }
+      })
+    })
+    console.log(haveUser);
+    if (haveUser) {
+      var userUpdateMessage = await new Promise((resolve, reject) => {
+        Usermsg.update({openId: data.openId},{
+          name: data.name,
+          sex: data.sex,
+          grade: data.grade,
+          major: data.major,
+          college: data.college,
+          phoneNum: data.phoneNum,
+          level: data.level,
+          reason: data.reason,
+          introduce: data.introduce,
+          otherReason: data.otherReason,
+        },(err) => {
+          if (!err) {
+            resolve('ok');
+          } else {
+            reject(err);
+          }
+        })
+      });
+      
+    }else{
+      const usermsg = new Usermsg({
+        openId: data.openId,
+        name: data.name,
+        sex: data.sex,
+        grade: data.grade,
+        major: data.major,
+        college: data.college,
+        phoneNum: data.phoneNum,
+        level: data.level,
+        reason: data.reason,
+        introduce: data.introduce,
+        otherReason: data.otherReason,
+      });
+      usermsg.save();
+    }
     ctx.body = 'ok';
   }
   async getUsermsg() {
